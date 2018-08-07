619e20a3acf8367ce05128af2b20281901d679dc
marcush12 <marcos012santos@gmail.com>
2018-08-07 19:26:28 -0300

    novamente

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -39,7 +39,7 @@
                             @endif
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-outline-primary btn-lg">Perguntar</button>
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Perguntar novamente</button>
                         </div>
                    </form>
                 </div>
