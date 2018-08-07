27b0268b7f4eee3b6c006a4b56a5eb9444e8684c
marcush12 <marcos012santos@gmail.com>
2018-08-07 19:18:51 -0300

    last

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -39,7 +39,7 @@
                             @endif
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Perguntar agora</button>
                         </div>
                    </form>
                 </div>
