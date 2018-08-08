d418ad5d4fe33e249c9f5e94de4424fa27f53bab
marcush12 <marcos012santos@gmail.com>
2018-08-08 12:06:26 -0300

    implemented show with slug

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('questions', 'QuestionsController');
+Route::resource('questions', 'QuestionsController')->except('show');//tirou a show para incluir a slug abaixo
+Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
