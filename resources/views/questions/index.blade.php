2d408849ed49b739b3ee93cb95c7b764efbfc06f
marcush12 <marcos012santos@gmail.com>
2018-08-08 09:26:40 -0300

    form validation and store the question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -15,7 +15,9 @@
                 </div>
 
                 <div class="card-body">
-                   @foreach ($questions as $question)
+                    @include('layouts._messages')
+
+                    @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
@@ -39,7 +41,7 @@
                             </div>
                         </div>
                         <hr>
-                   @endforeach
+                    @endforeach
 
                     <div class="mx-auto">
                         {{ $questions->links() }}
