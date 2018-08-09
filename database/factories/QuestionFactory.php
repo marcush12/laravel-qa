24bafc6feba5ec4f03178143f4df778ddd27e83d
marcush12 <marcos012santos@gmail.com>
2018-08-09 10:38:30 -0300

    rename answers column

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\Question::class, function (Faker $faker) {
         'title'=>rtrim($faker->sentence(rand(5,10)), "."),
         'body'=> $faker->paragraphs(rand(3,7), true), //true makes a string instead of an array
         'views'=> rand(0,10),
-        'answers'=> rand(0,10),
+        'answers_count'=> rand(0,10),
         'votes'=> rand(-3,10) //negative for unlikes
     ];//rtrim eliminates end dot . in the sentence cause it is a title
 });
