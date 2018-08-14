caa515a1b68e8a5a84c5d71b90f8d7b6a4437fcf
marcush12 <marcos012santos@gmail.com>
2018-08-14 10:07:23 -0300

    lesson 40

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -18,15 +18,25 @@
                           <a title="Esta resposta não foi útil" class="vote-down off">
                               <i class="fas fa-caret-down fa-3x"></i>
                           </a>
-                          <a title="Marcar como melhor resposta" 
-                            class="{{$answer->status}} mt-2"
-                            onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                            >
-                            <i class="fas fa-check fa-2x"></i>
-                          </a>
-                          <form id='accept-answer-{{ $answer->id }}' action="{{ route('answers.accept', $answer->id) }}" method='post' style="display:none;">
-                            @csrf
-                          </form>
+                          @can('accept', $answer)
+                            <a title="Marcar como melhor resposta" 
+                                class="{{$answer->status}} mt-2"
+                                onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
+                                >
+                                <i class="fas fa-check fa-2x"></i>
+                            </a>
+                            <form id='accept-answer-{{ $answer->id }}' action="{{ route('answers.accept', $answer->id) }}" method='post' style="display:none;">
+                                @csrf
+                            </form>
+                          @else
+                            @if($answer->is_best) 
+                                <a title="O autor da pergunta aceitou esta resposta como a melhor" 
+                                    class="{{$answer->status}} mt-2"
+                                    >
+                                    <i class="fas fa-check fa-2x"></i>
+                                </a>
+                            @endif
+                          @endcan
                       </div>
                       <div class="media-body">
                           {!! $answer->body_html !!}
